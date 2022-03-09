4bf8c0d5880442f887d64f0f5cfc48cf630c0d69
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-03-09 20:25:42 +0100

    Ajout de test, coorections de certaines méthodes, anticipation de mauvaises utilisations

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ class GrapheOriente():
     
     def supprimer_sommet(self, sommet):
         try:
+            all = self.predecesseurs(sommet)
+            for i in range(len(all)):
+                self.supprimer_arc(all[i],sommet)
             del self.graph[sommet]
         except:
             print("Sommet introuvable, merci de vérifier l'ortographe")
@@ -54,14 +57,18 @@ class GrapheOriente():
         return cpt 
 
     def successeurs(self, sommet):
-        return self.graph[sommet]
+        try: return self.graph[sommet]
+        except: print("Le sommet spécifié n'existe pas !")
 
     def predecesseurs(self, sommet):
-        l = []
-        for v in self.graph.keys():
-            if sommet in self.graph[v]:
-                l.append(v)
-        return l 
+        try:
+            l = []
+            for v in self.graph.keys():
+                if sommet in self.graph[v]:
+                    l.append(v)
+            return l 
+        except:
+            print("Le sommet spécifié n'existe pas !")
 
     def matrice(self):
         sommets = self.sommets()
@@ -96,14 +103,62 @@ class GrapheOriente():
 
 
 if __name__ == '__main__':
-    A = GrapheOriente()
-    A.genereSommets()
-    print(A.get_dico())
-    A.ajouter_arc('B', 'A')
-    A.ajouter_arc('C', 'A')
-    A.ajouter_arc('B','C')
-    A.ajouter_arc('D', 'A')
-    print(A.successeurs('B'))
-
-
-    
+    pause, skip = False, False
+    skip = False 
+    asw = input("Dans l'objectif de rendre les test plus lisibles, voulez vous utiliser la fonction de séparation, vous devrez appuyez sur 'Entrer' entre chaque série de test, skip permet d'eviter les test de départ (oui/non/skip)\n>>>")
+    if asw == 'oui' or asw == 'Oui' or asw == 'OUI':
+        pause = True 
+    if asw == 'skip':
+        skip = True 
+    if not skip:
+        print('\n----- Creation du graphe, ajout de sommet et arcs-----\n')
+        test = GrapheOriente()
+        test.ajouter_sommet('A')
+        test.ajouter_sommet('B')
+        test.ajouter_sommet('C')
+        print(test.get_dico()) 
+        print('Ajout des arcs :\n')
+        test.ajouter_arc('A','B')
+        test.ajouter_arc('A','C')
+        test.ajouter_arc('B','C')
+        print(test)
+        if pause : input('\nSuppression des arcs et sommets [ENTRER]\n')
+        test.supprimer_sommet('A')
+        print(test)
+        print(test.get_dico()) #on constate la suppression des arcs liés au sommet 'A' 
+        print("\n--on constate la suppression des arcs liés au sommet 'A'--\n")
+        test.supprimer_arc('B','C')
+        print(test)
+        
+        if pause: input('\nNous allons refaire un graph propre [ENTRER]\n')
+        new = GrapheOriente()
+        new.genereSommets(5) #cette méthode permet de générer 5 sommets dans l'ordre alphabétique 
+        print(new.get_dico())
+        print(new.sommets())
+        print(new.nb_sommets())
+        new.ajouter_arc("B","A")
+        new.ajouter_arc('D','A')
+        new.ajouter_arc('B','D')
+        print('\n')
+        print(new)
+        print(new.nb_arcs())
+        print('\n')
+        print(new.successeurs('B'))
+        print(new.predecesseurs('A'))
+        if pause: input("\n-----Génération d'une matrice [ENTRER]-----")
+        print(new.matrice())
+    else:
+        print('\n-----skipped-----\n')
+    print("L'objectif est ici de casser la class\n")
+    destroy = GrapheOriente()
+    print('test de toutes les méthodes avec un graph vide :\n')
+    print(destroy.get_dico())
+    print(destroy.matrice())
+    print(destroy.nb_arcs())
+    print(destroy.nb_sommets())
+    print('Test avec des arguments mauvais :')
+    destroy.ajouter_arc('untruc','innexistant')
+    destroy.supprimer_sommet('rienzoifn')
+    destroy.supprimer_arc('le néant',"un autre néant")
+    destroy.successeurs('nimp')
+    destroy.predecesseurs('nimp') 
